0a527ac734f33c41d3ed374ef991878d0e235c31
CrispyMiatr <maxivowes@gmail.com>
2025-11-18 00:32:41 +0100

    fix(Root): remove faulty, unused import

--- front-end/src/modules/pages/Root/Root.tsx
+++ front-end/src/modules/pages/Root/Root.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { App } from '../App';
 
 const queryClient = new QueryClient()
-// import '../App/i18n.config.ts';
 
 export const Root = () => {
     return (
